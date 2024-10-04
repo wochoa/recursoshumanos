e6a9d1a3ff9a9d7b87b737a3d56a2080b03279e6
wochoa <woalvarado1@gmail.com>
2024-10-03 19:04:15 -0500

    v27

--- app/Http/Controllers/LicenciaspersonalController.php
+++ app/Http/Controllers/LicenciaspersonalController.php
@@ -55,31 +55,72 @@ class LicenciaspersonalController extends Controller
     {
 
             $datos=$request->all();
+            $add=($request->tipofrm==1)?new Licenciaspersonal():Licenciaspersonal::find($request->id);
+            $ndias=$request->ndias;
 
-            if($request->vacaciones=='VACACIONES')
-            {
-                $result=$this->verifica_vacaciones($request->dni,$request->ndias);
-                if($result<0)
-                {
-                    return response()->json(["status"=>0,'message'=>'No cuenta con vacaciones, según los '.$request->ndias.' días solicitados'], 200);
-                }
-                else{
-                    return response()->json(["status"=>1,'message'=>'Si contó con vacaciones'], 200);
-                }
-            }
+                if($request->vacaciones=='VACACIONES')
+                    {
+                        if($request->tipofrm==2)// cuando es para actualizar
+                        {
+                            $dia_ant=intval($add->ndias);// ndias que existe en bd
+                            $dia_act=intval($request->ndias);// nuevo ndias actualizado
+                            $resta=$dia_ant-$dia_act;
+                            
+                            if($resta>0){// se debe sumar la diferencia de dias en dias que quedan en vacaciones
+                                $buscar= Vacaciones::join('escalafon','vacaciones.idescalafon','=','escalafon.idescalafon')->where('dni',$request->dni)->first();
+                                $restadias=intval($buscar->rest_vacaciones)+intval($resta);
+                                // $ndias=$restadias;
 
-            $add=new Licenciaspersonal();
+                                $updvacas=Vacaciones::find($buscar->id);
+                                $updvacas->rest_vacaciones=$restadias;
+                                $updvacas->save();
+                            }
+                            else{// aqui se tiene que restar pero previo verificacion de vacaciones
+                                $cantidad=abs($resta);
+                                $buscar= Vacaciones::join('escalafon','vacaciones.idescalafon','=','escalafon.idescalafon')->where('dni',$request->dni)->first();
+                                $restadias=intval($buscar->rest_vacaciones)-intval($cantidad);
+                                // $ndias=$restadias;
+
+                                if($restadias<0)
+                                    {
+                                        return response()->json(["status"=>0,'message'=>'No cuenta con vacaciones, según los '.$request->ndias.' días solicitados'], 200);
+                                    }
+                                    else{
+                                        $updvacas=Vacaciones::find($buscar->id);
+                                        $updvacas->rest_vacaciones=$restadias;
+                                        $updvacas->save();
+                                    }
+
+                                
+
+                            }
+
+                        }
+                        else{ //cuando el registro es nuevo
+                            $result=$this->verifica_vacaciones($request->dni,$request->ndias);
+                            if($result<0)
+                            {
+                                return response()->json(["status"=>0,'message'=>'No cuenta con vacaciones, según los '.$request->ndias.' días solicitados'], 200);
+                            }
+                            else{
+                                return response()->json(["status"=>1,'message'=>'Si contó con vacaciones'], 200);
+                            }
+                        }
+                        
+                    }
+
             $add->fill($datos);
+            $add->ndias=$ndias;
             $add->save();
             return response()->json(["status"=>3,'message'=>'fue guardado con exito'], 200);
 
     }
     public function verifica_vacaciones($dni,$dias)
     {
-        $buscar= Vacaciones::rightJoin('escalafon','vacaciones.idescalafon','=','escalafon.idescalafon')
+        $buscar= Vacaciones::join('escalafon','vacaciones.idescalafon','=','escalafon.idescalafon')
                             ->where('dni',$dni)->first();
             // actualizamos el resta dias nuevo= total-resta-ndias // son los dias que quedan para el servidor
-            $restadias=intval($buscar->tot_vacaciones)-intval($buscar->rest_vacaciones)-intval($dias);
+            $restadias=intval($buscar->tot_vacaciones)-(intval($buscar->rest_vacaciones)-intval($dias));
             if($restadias>=0)
             {
                 $updvacas=Vacaciones::find($buscar->id);
